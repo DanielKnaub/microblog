6146f9807831db47bfd09ac001a34d335aa2dd2a
Daniel Knaub <knaubdev@gmail.com>
2023-04-09 12:46:31 +0000

    Third lesson completed

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username':'Daniil'}
+    user = {'username': 'Daniil'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -12,7 +14,7 @@ def index():
         },
         {
             'author': {'username': 'Susan'},
-            'body':'The Avengers mvie was so cool!'
+            'body': 'The Avengers mOvie was so cool!'
         },
         {
             'author': {'username': 'Ippolit'},
@@ -21,3 +23,12 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
